53eeda8decb29764b845a742a019bb25e4d4988c
Watson <watson1978@gmail.com>
2010-03-14 20:52:42 +0900

    Initial commit.

---
+++ macruby/objc-macruby.m
@@ -0,0 +1,15 @@
+/* filename : objc-macruby.m */
+/* Compile: gcc objc-macruby.m -framework Foundation -framework MacRuby -fobjc-gc */
+#import <Foundation/Foundation.h>
+#import <MacRuby/MacRuby.h>
+
+int main(void) {
+  id ruby;
+
+  ruby = [[MacRuby sharedRuntime] evaluateString:@"puts 'test'"];
+  ruby = [[MacRuby sharedRuntime] evaluateFileAtPath:@"fib.rb"];
+
+  NSNumber *Num = [NSNumber numberWithInt: 31];
+  NSLog(@"%@", [ruby performRubySelector:@selector(main) withArguments: (id)num, NULL]);
+  return 0;
+}
